04100c9bf04626f2271f9ad2d7a2618c1eac421d
Andreia Silva <andreia.silva@sensedia.com>
2020-08-18 21:07:43 -0300

    Ajustando purchase

--- purchase/pkg/api/routers/router.go
+++ purchase/pkg/api/routers/router.go
@@ -14,7 +14,7 @@ type SystemRoutes struct {
 
 func (sys *SystemRoutes) SetupHandler() http.Handler {
 	r := mux.NewRouter().PathPrefix("/purchase/api/v1").Subrouter()
-	r.HandleFunc("/buy", sys.purchaseHandler.Post).Methods(http.MethodPost, http.MethodOptions)
+	r.HandleFunc("/buy", sys.purchaseHandler.Buy).Methods(http.MethodPost, http.MethodOptions)
 	return r
 }
 
